cec08f2a8f42e85720390e81e232a7203dd120ec
寒露 <47081054+matsuyoido@users.noreply.github.com>
2021-01-17 14:18:11 +0900

    Add github pages でSPA をするための設定
    https://github.com/rafgraph/spa-github-pages
    を参考

--- develop/src/main/js/url.tsx
+++ develop/src/main/js/url.tsx
@@ -3,20 +3,21 @@ export default class UrlMap {
     private constructor() {}
 
     static readonly ROOMID_PARAM_NAME = 'roomID';
+    static readonly REPOSITORY_PATH = '/neu-react';
 
     static inputNameUrl(): string {
-        return '/enter';
+        return UrlMap.REPOSITORY_PATH + '/enter';
     }
     static generateInputNameUrl(roomId: string): string {
-        return `/enter?${UrlMap.ROOMID_PARAM_NAME}=${roomId}`;
+        return UrlMap.REPOSITORY_PATH + `/enter?${UrlMap.ROOMID_PARAM_NAME}=${roomId}`;
     }
 
     static readonly ROOMID_URI_VARIABLE_NAME = 'roomId';
     static playingUrl(): string {
-        return `/play/:${UrlMap.ROOMID_URI_VARIABLE_NAME}`;
+        return UrlMap.REPOSITORY_PATH + `/play/:${UrlMap.ROOMID_URI_VARIABLE_NAME}`;
     }
     static generatePlayingUrl(roomId: string): string {
-        return `/play/${roomId}`;
+        return UrlMap.REPOSITORY_PATH + `/play/${roomId}`;
     }
 
 };
